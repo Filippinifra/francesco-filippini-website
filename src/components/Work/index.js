aa384dbb1062e2bad2fcdc83813f6e1e02cac69d
filippinifra <francesco1.filippini@mail.polimi.it>
2020-04-05 17:43:48 +0200

    Create work component

---
+++ src/components/Work/index.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+import { Grid } from "@material-ui/core";
+
+import workList from "../../constants/commonsWork";
+
+import {
+  ContainerHome,
+  Text,
+  ContainerImg,
+  LogoImg,
+  StrongTitle,
+  MediumTitle,
+  ContainerGrid
+} from "./styled";
+
+const Work = () => (
+  <ContainerHome>
+    {workList.map(workExperience => {
+      const {
+        logoImg,
+        title,
+        position,
+        periodAndLocation,
+        textList
+      } = workExperience;
+      return (
+        <ContainerGrid>
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="flex-start"
+            spacing={5}
+          >
+            <Grid item xs={12} md={3}>
+              <ContainerImg>
+                <LogoImg src={logoImg} />
+              </ContainerImg>
+            </Grid>
+            <Grid item xs={12} md={9}>
+              <StrongTitle>{title}</StrongTitle>
+              <MediumTitle>{position}</MediumTitle>
+              <MediumTitle>{periodAndLocation}</MediumTitle>
+              {textList.map(paragraph => (
+                <Text>{paragraph.text}</Text>
+              ))}
+            </Grid>
+            <br />
+          </Grid>
+        </ContainerGrid>
+      );
+    })}
+  </ContainerHome>
+);
+
+export default Work;
