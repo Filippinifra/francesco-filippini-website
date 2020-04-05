765f2ba10d04131490e8843519c731fd53574f42
filippinifra <francesco1.filippini@mail.polimi.it>
2020-04-05 17:43:04 +0200

    Adding commons of work and education

---
+++ src/constants/commonsEducation.js
@@ -0,0 +1,37 @@
+import politecnicoLogo from "../img/politecnicoMilano.png";
+import fracastoroLogo from "../img/liceoFracastoro.png";
+import stJonesLogo from "../img/stJones.png";
+
+const educationList = [
+  {
+    logoImg: politecnicoLogo,
+    title: "Politecnico Di Mialno",
+    extraInfo: "Bachelor of Computer Engineering",
+    periodAndLocation: "Milan, 2015 - 2020",
+    textList: [
+      {
+        text:
+          "Courses attended: Fundamentals of Computer Science - Fundamentals of Internet and Networks - Computer Architecture and Operating Systems - Algorithms and Principles of Computer Science - Fundamentals of Automatic - Probability and Statistics for Information Technology - Databases - Logical Networking - Hypermedia Applications - Software Engineering - Information System"
+      }
+    ],
+    images: []
+  },
+  {
+    logoImg: fracastoroLogo,
+    title: "Liceo Scientifico Girolamo Fracastoro",
+    extraInfo: "Italian High School Diploma",
+    periodAndLocation: "Verona, 2010 - 2015",
+    textList: [],
+    images: []
+  },
+  {
+    logoImg: stJonesLogo,
+    title: "St. John's University",
+    extraInfo: "English 1 month course",
+    periodAndLocation: "New York, July 2012",
+    textList: [],
+    images: []
+  }
+];
+
+export default educationList;
---
+++ src/constants/commonsWork.js
@@ -0,0 +1,45 @@
+import finleapLogo from "../img/finleap.png";
+import smbLogo from "../img/smb.svg";
+
+const workList = [
+  {
+    logoImg: finleapLogo,
+    title: "Finleap",
+    position: "Junior Frontend Developer",
+    periodAndLocation: "Milan, October 2019 - April 2020",
+    textList: [
+      {
+        text:
+          "My team builds SaaS software for 'Cessione del quinto', an Italian Administration process to obtain a loan."
+      },
+      { text: "I contributed by building parts of the website frontend." },
+      {
+        text:
+          "I work in React and I learnt a lot both on the tech side (software development best practices and team work) and on the personal side as I got to enjoy a young workspace and disrupting company environment."
+      }
+    ],
+    images: []
+  },
+  {
+    logoImg: smbLogo,
+    title: "Saint Mary Boulevard",
+    position: "Manager and Founder",
+    periodAndLocation: "Verona, March 2016 - March 2020",
+    textList: [
+      {
+        text: "I started this project when I was 20 together with my brother."
+      },
+      {
+        text:
+          "Our concept was to create a clothing brand after the name of the town where we lived. It started as a fun side-project, but it became a solid reality and one of the most popular brand in Verona and near towns. We also partnered with schools and small-to-medium sized associations to create customised clothing."
+      },
+      {
+        text:
+          "During these years I learnt how to manage a business and all the components that revolve around it: relationship with customers and suppliers, taxes, marketing tactics and managing my own business."
+      }
+    ],
+    images: []
+  }
+];
+
+export default workList;
