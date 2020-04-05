0bf94d5340c890ada83e7241e3afcaa301b4f72c
filippinifra <francesco1.filippini@mail.polimi.it>
2020-04-05 13:35:13 +0200

    Mapping texts in commonsFooter to render

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import Drawer from "@material-ui/core/Drawer";
 
-import { ButtonModal, ContainerModal, TextRow, Link } from "./styled";
+import { footerLabel, contentModal } from "../../constants/commonsFooter";
+
+import { ButtonModal, ContainerModal, TextRow } from "./styled";
 
 const Footer = () => {
   const [modalOpen, setModalOpen] = useState(false);
@@ -18,41 +20,13 @@ const Footer = () => {
 
   return (
     <>
-      <ButtonModal onClick={openModalWithDelay}>
-        {"Created from scratch using React / Thanks"}
-      </ButtonModal>
+      <ButtonModal onClick={openModalWithDelay}>{footerLabel}</ButtonModal>
       <Drawer anchor={"bottom"} open={modalOpen} onClose={closeModal}>
         <ContainerModal>
           <div>
-            <TextRow>
-              Made with <span style={{ color: "#e25555" }}>&hearts;</span> using
-            </TextRow>
-            <TextRow>
-              <Link href="https://reactjs.org/" target="_blank">
-                React
-              </Link>
-              {" - "}
-              <Link href="https://material-ui.com/" target="_blank">
-                Material UI
-              </Link>
-            </TextRow>
-            <TextRow>
-              <Link href="https://flaticon.com/" target="_blank">
-                Flaticon
-              </Link>
-              {" - "}
-              <Link href="https://styled-components.com/" target="_blank">
-                Styled Components
-              </Link>
-            </TextRow>
-            <TextRow>
-              <Link
-                href="https://github.com/Filippinifra/francesco-filippini-website"
-                target="_blank"
-              >
-                Click for website repo
-              </Link>
-            </TextRow>
+            {contentModal.map(element => (
+              <TextRow>{element.content}</TextRow>
+            ))}
           </div>
         </ContainerModal>
       </Drawer>
