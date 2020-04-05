663f5a81b3fbb42c4b49b862b0e7897bd06f2a5a
filippinifra <francesco1.filippini@mail.polimi.it>
2020-04-05 02:44:16 +0200

    Adding global css using styled components

---
+++ src/styled.js
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from "styled-components";
+
+import colors from "./constants/colors";
+import typography from "./constants/typography";
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        margin: 0;
+        font-family: ${typography.fontFamily} !important;
+        background-color: ${colors.bodyBgColor};
+        height: 100vh;
+        width: 100%;
+        overflow:hidden;
+    }
+  
+    code {
+        font-family: ${typography.fontFamily} !important;
+    }
+
+    *{
+        ::selection{
+            background-color: ${colors.selectionBgColor};
+            color: ${colors.selectionColor};
+        }
+    }
+`;
+
+export default GlobalStyle;
