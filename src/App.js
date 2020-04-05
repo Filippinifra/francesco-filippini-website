3161289dd64fac8f53505db00f9daedf99f6db27
filippinifra <francesco1.filippini@mail.polimi.it>
2020-04-05 02:46:10 +0200

    Moving logic of Swapper into App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+import {
+  effectTransformation,
+  lifeTimeAnimation
+} from "./constants/animationSettings";
+
+import { ThemeProvider } from "@material-ui/core/styles";
+
+import mapNameComponent from "./constants/mapNameComponent";
+
+import PageLayout from "./components/PageLayout";
+
+import theme from "./constants/theme";
+import CurtainOpening from "./components/CurtainOpening";
+
+import { checkNewSession, setExistingSession } from "./utils/session";
+
+const App = () => {
+  const [facesSwapper, setFacesSwapper] = useState(["home", "home"]);
+  const [indexEffectArray, setIndexEffectArray] = useState(0);
+  const [animationIsActive, setAnimationActive] = useState(false);
+
+  const isNewSession = checkNewSession();
+
+  const [pageIsLoading, setPageLoading] = useState(isNewSession);
+
+  const setNextDirectionFlip = () => {
+    const nextIndexEffect = indexEffectArray + 1;
+    nextIndexEffect >= effectTransformation.length
+      ? setIndexEffectArray(0)
+      : setIndexEffectArray(nextIndexEffect);
+  };
+
+  const prepareNewAnimation = value => {
+    setTimeout(() => {
+      setFacesSwapper([value, value]);
+      setAnimationActive(false);
+      setNextDirectionFlip();
+    }, lifeTimeAnimation);
+  };
+
+  const startAnimation = value => {
+    setFacesSwapper([facesSwapper[0], value]);
+    setAnimationActive(true);
+    prepareNewAnimation(value);
+  };
+
+  const handleMenuClick = value => {
+    const currentElementName = getCurrentElementName();
+    if (!animationIsActive && value !== currentElementName) {
+      startAnimation(value);
+    }
+  };
+
+  const getFaceComponent = numberFace => {
+    const namePage = facesSwapper[numberFace];
+    const componentLinked = getContentElement(namePage);
+    return componentLinked;
+  };
+
+  const getContentElement = pageLabel => mapNameComponent[pageLabel];
+  const getCurrentElement = () => getFaceComponent(0);
+  const getNextElement = () => getFaceComponent(1);
+  const getDirectionFlip = () => effectTransformation[indexEffectArray];
+  const getCurrentElementName = () => facesSwapper[1];
+
+  useEffect(() => {
+    setTimeout(() => {
+      setPageLoading(false);
+      setExistingSession();
+    }, 2000);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CurtainOpening isloading={pageIsLoading}>
+        <PageLayout
+          handleChange={handleMenuClick}
+          currentSection={getCurrentElementName()}
+          currentElement={getCurrentElement()}
+          nextElement={getNextElement()}
+          animationIsActive={animationIsActive}
+          rotationString={getDirectionFlip()}
+        />
+      </CurtainOpening>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
