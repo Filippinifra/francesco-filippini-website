3fbcb6fc3223d056f250dcda0a4a713577dca1c5
filippinifra <francesco1.filippini@mail.polimi.it>
2020-04-05 17:42:47 +0200

    Remap right content

--- src/constants/mapNameComponent.js
+++ src/constants/mapNameComponent.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import Home from "../components/Home";
 import Contacts from "../components/Contacts";
+import Work from "../components/Work";
+import Education from "../components/Education";
 
 const mapNameComponent = {
   home: <Home />,
-  education: <div>schoollll</div>,
-  work: <div>workkkk</div>,
+  education: <Education />,
+  work: <Work />,
   contacts: <Contacts />
 };
 
