837f081e5fd77c8b55c9ea2a060859c16ffe9af5
filippinifra <francesco1.filippini@mail.polimi.it>
2020-04-08 20:09:14 +0200

    Change palette colors

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,32 +1,35 @@
-const WHITE = "white";
-const BLUE = "#5294e2";
-const LIGHT_GREY = "#7c818c";
-const MIDDLE_GREY = "#4b5162";
-const DARK_GREY = "#383c4a";
+const TRANSPARENT = "transparent";
 const BLACK = "black";
-const CREAM = "#bdbec0";
+const WHITE = "white";
+
+const BRIGHT_NAVY_BLUE = "#1C77C3";
+const MAXIMUM_BLUE = "#39A9DB";
+const SEA_SERPENT = "#40BCD8";
+
+const DEEP_SAFFRON = "#F39237";
+const PERSIAN_RED = "#D63230";
 
 const colors = {
-  bodyBgColor: MIDDLE_GREY,
-  headerBgColor: DARK_GREY,
-  headerColor: LIGHT_GREY,
+  bodyBgColor: BRIGHT_NAVY_BLUE,
+  headerBgColor: BRIGHT_NAVY_BLUE,
+  headerColor: WHITE,
   headerShade: BLACK,
-  flippingElementBgColor: LIGHT_GREY,
+  flippingElementBgColor: SEA_SERPENT,
   flippingElementColor: BLACK,
   flippinImgShadowColor: BLACK,
-  cardBgColor: CREAM,
-  cardBorderColor: MIDDLE_GREY,
+  cardBgColor: MAXIMUM_BLUE,
+  cardBorderColor: BLACK,
   cardLinkColor: BLACK,
-  footerColor: LIGHT_GREY,
-  footerBgColor: DARK_GREY,
-  footerShade: BLACK,
-  modalLinkColor: DARK_GREY,
-  modalColor: DARK_GREY,
-  modalBgColor: BLUE,
-  selectionBgColor: BLUE,
-  selectionColor: WHITE,
-  curtainBgColor: BLUE,
-  curtainColor: WHITE
+  footerColor: BLACK,
+  footerBgColor: DEEP_SAFFRON,
+  footerShade: TRANSPARENT,
+  modalLinkColor: PERSIAN_RED,
+  modalColor: BLACK,
+  modalBgColor: DEEP_SAFFRON,
+  selectionBgColor: PERSIAN_RED,
+  selectionColor: DEEP_SAFFRON,
+  curtainBgColor: DEEP_SAFFRON,
+  curtainColor: PERSIAN_RED,
 };
 
 export default colors;
