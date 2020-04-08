17259a5ba185f5509b1704624ce598e63f19503a
filippinifra <francesco1.filippini@mail.polimi.it>
2020-04-08 19:38:27 +0200

    Change home text

--- src/constants/commonsHome.js
+++ src/constants/commonsHome.js
@@ -4,42 +4,34 @@ import curriculumImg from "../img/FrancescoFilippiniCV.png";
 export const cardDownloadSettings = {
   curriculumPdf: curriculumPdf,
   curriculumImg: curriculumImg,
-  downloadLabel: "FilippiniFCV.pdf"
+  downloadLabel: "FilippiniFCV.pdf",
 };
 
 export const textFirstParagraph = [
   {
-    text: `Hello World! I'm Francesco, I lived in Verona for 18 years, then i
-          moved to Milan.`
+    text: `Hello World! I'm Francesco, I was born in Verona where I lived for 18 years, before moving to Milan.`,
   },
   {
-    text: `I love everything about technology: from robots, superstore with no
-          cashiers, bluetooth headphones to Elon Musk's rockets. Every aspect of
-          technology makes me crazy. When something make me feel curious I want
-          to know how it is made, how it works and the logic behind.`
+    text: `I love everything about technology: from robots to cashier-free stores, from bluetooth headphones to Elon Musk's rockets.
+      Every aspect of technology makes me crazy and when something makes me feel curious I want to know how it is made, how it works and what’s the logic behind.
+      One of my favourite piece of technology is web products like websites and web apps.`,
   },
   {
-    text: `One of my favourite piece of technology is web products like websites
-          and apps. Landing on website is like tasting a candy. Enter on home
-          page is like when you remove the candy wrap. Scrolling pages and watch
-          animations is like when you enjoy the candy.`
-  }
+    text: `Landing on a website is like tasting a candy. Entering the home page is like when you remove the candy wrap. Scrolling pages and watch animations is like when you enjoy the candy.`,
+  },
 ];
 
 export const textSecondParagraph = [
   {
-    text: `My real first education process started in Politecnico di Milano. I
-    attended Software Engineer lessons. I loved my university, expecially
-    I was excited to work on coding projects.`
+    text: `My software education started in Politecnico di Milano where I graduated with a Bachelor in Computer Engineering.
+    I loved my university, but I was especially excited about working on coding projects where I always got my best results.`,
   },
   {
-    text: `During the university period I started an extra experience. Me and my
-    brother founded Saint Mary Boulevard, an Italian Startup created from
-    scratch specialized in selling clothes.`
+    text: `During my university time, I started a side project: me and my brother founded Saint Mary Boulevard, a business specialized in selling clothes and branding our local community.
+    It then grew up to be a major reality in the clothing space of my city.`,
   },
   {
-    text: `My first working experience was in 2019: I took Software Engineer
-    Intern position in finleap, a fintech company. This experience was
-    wondering, I learn Javascript and React and I felt in love.`
-  }
+    text: `My first working experience was in 2019: I was a Software Engineer Intern in Finleap, a fintech company. 
+    During this experience I was able to specialise in frontend, learning React and I felt in love with it.`,
+  },
 ];
