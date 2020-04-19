e6aac144247ed31bfbe98565feeb14f3fac9be34
filippinifra <francesco1.filippini@mail.polimi.it>
2020-04-19 19:35:44 +0200

    Restyling header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,110 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { NavigationBar, NavigationItem } from "./styled";
+import { withSize } from "react-sizeme";
 
-import HomeIcon from "@material-ui/icons/Home";
-import SchoolIcon from "@material-ui/icons/School";
-import ContactsIcon from "@material-ui/icons/Contacts";
-import WorkIcon from "@material-ui/icons/Work";
+import {
+  ContainerNavbar,
+  NavbarHeader,
+  GridContainer,
+  LogoImg,
+  NavButton,
+  RightGrid,
+  LeftGrid,
+  GridItem,
+  NavMobileButton,
+  ContainerMobileLink,
+  BorderContainer,
+} from "./styled";
 
-const Header = ({ handleChange, currentSection }) => (
-  <NavigationBar
-    value={currentSection}
-    onChange={(e, value) => handleChange(value)}
-  >
-    <NavigationItem label="Home" value="home" icon={<HomeIcon />} />
-    <NavigationItem label="Career" value="work" icon={<WorkIcon />} />
-    <NavigationItem label="Education" value="education" icon={<SchoolIcon />} />
-    <NavigationItem label="Contacts" value="contacts" icon={<ContactsIcon />} />
-  </NavigationBar>
-);
+import FFLogo from "../../img/myLogo.png";
+import lightOn from "../../img/lightOn.svg";
+import lightOff from "../../img/lightOff.svg";
 
-export default Header;
+import {
+  aboutLabel,
+  educationLabel,
+  workLabel,
+  contactsLabel,
+} from "../../constants/mapNameComponent";
+
+import sizes from "../../constants/screenSizes";
+
+import Hamburger from "./Hamburger";
+
+const Header = ({ handleNavClick, handleLightClick, lightIsOn, size }) => {
+  const [hamburgerActive, setHamburgerActive] = useState(false);
+  const getLightImage = () => (lightIsOn ? lightOn : lightOff);
+  const handleClick = (name) => {
+    if (hamburgerActive === true) setHamburgerActive(false);
+    handleNavClick(name);
+  };
+
+  const handleHamburgerClick = () => {
+    hamburgerActive === true
+      ? setHamburgerActive(false)
+      : setHamburgerActive(true);
+  };
+
+  return (
+    <NavbarHeader>
+      <ContainerNavbar>
+        <GridContainer
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+        >
+          <GridItem item xs={6} sm={3}>
+            <LeftGrid>
+              <LogoImg src={FFLogo} />
+              <LogoImg src={getLightImage()} onClick={handleLightClick} />
+            </LeftGrid>
+          </GridItem>
+          <GridItem item xs={6} sm={9}>
+            <RightGrid>
+              {size.width > sizes.headerStep ? (
+                <>
+                  <NavButton onClick={() => handleClick(aboutLabel)}>
+                    {aboutLabel}
+                  </NavButton>
+                  <NavButton onClick={() => handleClick(workLabel)}>
+                    {workLabel}
+                  </NavButton>
+                  <NavButton onClick={() => handleClick(educationLabel)}>
+                    {educationLabel}
+                  </NavButton>
+                  <NavButton onClick={() => handleClick(contactsLabel)}>
+                    {contactsLabel}
+                  </NavButton>
+                </>
+              ) : (
+                <Hamburger
+                  handleHamburgerClick={handleHamburgerClick}
+                  active={hamburgerActive}
+                />
+              )}
+            </RightGrid>
+          </GridItem>
+        </GridContainer>
+      </ContainerNavbar>
+      <ContainerMobileLink hamburgerActive={hamburgerActive}>
+        <BorderContainer>
+          <NavMobileButton onClick={() => handleClick(aboutLabel)}>
+            {aboutLabel}
+          </NavMobileButton>
+          <NavMobileButton onClick={() => handleClick(workLabel)}>
+            {workLabel}
+          </NavMobileButton>
+          <NavMobileButton onClick={() => handleClick(educationLabel)}>
+            {educationLabel}
+          </NavMobileButton>
+          <NavMobileButton onClick={() => handleClick(contactsLabel)}>
+            {contactsLabel}
+          </NavMobileButton>
+        </BorderContainer>
+      </ContainerMobileLink>
+    </NavbarHeader>
+  );
+};
+
+export default withSize()(Header);
--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -1,20 +1,95 @@
 import Styled from "styled-components";
 
-import { BottomNavigation, BottomNavigationAction } from "@material-ui/core";
-
 import colors from "../../constants/colors";
 
-export const NavigationBar = Styled(BottomNavigation)`
-    background-color: ${colors.headerBgColor} !important;
-    height: 70px !important;
+import { Grid } from "@material-ui/core";
+
+export const GridItem = Styled(Grid)`
+    height:100%;
+`;
+
+export const NavbarHeader = Styled.div`
+    background-color: ${colors.headerBgColor};
+    height: auto;
     -webkit-box-shadow: 0px 5px 64px -9px ${colors.headerShade};
     -moz-box-shadow: 0px 5px 64px -9px ${colors.headerShade};
     box-shadow: 0px 5px 64px -9px ${colors.headerShade};
-    position: fixed;
-    width: 100%;
 `;
 
-export const NavigationItem = Styled(BottomNavigationAction)`
-    background-color: ${colors.headerBgColor} !important;
-    color: ${colors.headerColor} !important;
+export const ContainerNavbar = Styled.div`
+    background-color: ${colors.headerBgColor};
+    color: ${colors.headerColor};
+    max-width: 1000px;
+    margin: auto;
+    height: 100%;
+`;
+
+export const GridContainer = Styled(Grid)`
+    height: 100%;
+`;
+
+export const LogoImg = Styled.img`
+    height: 30px;
+    margin: 10px;
+`;
+
+export const NavButton = Styled.button`
+    background-color:transparent;
+    height: 100%;
+    padding: 16px 25px;
+    letter-spacing: 2px;
+    font-size: 15px;
+    border: 0;
+    color: ${colors.headerColor};
+    outline: none;
+    cursor: pointer;
+    transition: background 0.6s;
+    text-transform: uppercase;
+
+    :hover {
+        background: ${colors.headerColor};
+        color: ${colors.headerBgColor};
+    }
+`;
+
+export const RightGrid = Styled.div`
+    text-align: right;
+    margin-right: 15px;
+    height: 50px;
+`;
+
+export const LeftGrid = Styled.div`
+    text-align: left;
+    margin-left: 15px;
+    height: 50px;
+`;
+
+export const NavMobileButton = Styled.button`
+    background-color: ${colors.headerBgColor};
+    height: 40px;
+    padding: 16px auto;
+    letter-spacing: 2px;
+    font-size: 15px;
+    border: 0;
+    color: ${colors.headerColor};
+    outline: none;
+    cursor: pointer;
+    transition: background 0.6s;
+    text-transform: uppercase;
+    width: 100%;
+
+    :hover {
+        background: ${colors.headerColor};
+        color: ${colors.headerBgColor};
+    }
+`;
+
+export const ContainerMobileLink = Styled.div`
+    max-height: ${(props) => (props.hamburgerActive ? "164px" : "0")};
+    transition: max-height 0.6s ease-in-out;
+    overflow: hidden;
+`;
+
+export const BorderContainer = Styled.div`
+    border: 2px ${colors.headerColor} solid;
 `;
