e1b0f41fe80e48f03f2ac932a0f03a953d2676f2
filippinifra <filippinifra123@gmail.com>
2020-09-07 14:51:35 +0200

    Add router and routes to index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { HOME_PATH, HOME_RELATIVE_PATH } from "constants/paths";
+import { defaultLanguage } from "constants/languages";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route exact path={HOME_PATH}>
+          <App />
+        </Route>
+        <Route path="*">
+          <Redirect to={HOME_RELATIVE_PATH + defaultLanguage} />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
