3f017d8003cc5401f54027476e3273db064f3aed
filippinifra <filippinifra123@gmail.com>
2020-09-08 13:44:56 +0200

    Adding dependencies in text footer

--- src/text/textFooter.js
+++ src/text/textFooter.js
@@ -1,33 +1,40 @@
+const libraries = [
+  [
+    { label: "React", link: "https://reactjs.org/" },
+    { label: "Material UI", link: "https://material-ui.com/" },
+  ],
+  [
+    { label: "Flaticon", link: "https://flaticon.com/" },
+    { label: "Styled Components", link: "https://styled-components.com/" },
+  ],
+  [
+    { label: "i18next", link: "https://www.i18next.com/" },
+    { label: "React Router Dom", link: "https://reactrouter.com/" },
+  ],
+  [
+    {
+      label: "React Scroll",
+      link: "https://www.npmjs.com/package/react-scroll",
+    },
+    {
+      label: "React SizeMe",
+      link: "https://github.com/ctrlplusb/react-sizeme",
+    },
+  ],
+];
+
 export const textFooter = {
   en: {
     footerLabel: "Created from scratch using React",
     firstText: "Made with ❤️ using",
-    libraries: [
-      [
-        { label: "React", link: "https://reactjs.org/" },
-        { label: "Material UI", link: "https://material-ui.com/" },
-      ],
-      [
-        { label: "Flaticon", link: "https://flaticon.com/" },
-        { label: "Styled Components", link: "https://styled-components.com/" },
-      ],
-    ],
+    libraries,
     repoText: "Click for website repo",
     repoLink: "https://github.com/Filippinifra/francesco-filippini-website",
   },
   it: {
     footerLabel: "Creato da zero usando React",
     firstText: "Fatto con ❤️ usando",
-    libraries: [
-      [
-        { label: "React", link: "https://reactjs.org/" },
-        { label: "Material UI", link: "https://material-ui.com/" },
-      ],
-      [
-        { label: "Flaticon", link: "https://flaticon.com/" },
-        { label: "Styled Components", link: "https://styled-components.com/" },
-      ],
-    ],
+    libraries,
     repoText: "Clicca qui per vedere la repo",
     repoLink: "https://github.com/Filippinifra/francesco-filippini-website",
   },
