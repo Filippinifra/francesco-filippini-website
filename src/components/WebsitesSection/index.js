b484b196673f7fbd5b272592c33363a2c5f58aa3
filippinifra <filippinifra123@gmail.com>
2020-09-14 23:51:32 +0200

    Fix popper in Website section

--- src/components/WebsitesSection/index.js
+++ src/components/WebsitesSection/index.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 
 import Lottie from 'react-lottie';
 import animationData from './rainbow-animation.json';
@@ -30,6 +30,8 @@ export const WebsitesSection = ({ lightIsOn, colors }) => {
   const buttonRef = useRef(null);
   const [isTooltipVisible, setTooltipVisible] = useState(true);
 
+  const [tooltipPlacement, setTooltipPlacement] = useState('top');
+
   const defaultOptions = {
     loop: true,
     autoplay: true,
@@ -43,6 +45,24 @@ export const WebsitesSection = ({ lightIsOn, colors }) => {
     setTimeout(() => setTooltipVisible(false), tooltipWebsiteRemoveAfterHover);
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (buttonRef && buttonRef.current) {
+        const top = buttonRef.current.getBoundingClientRect().top;
+        if (top < 85 && tooltipPlacement === 'top') {
+          setTooltipPlacement('bottom');
+        }
+        if (top >= 85 && tooltipPlacement === 'bottom') {
+          setTooltipPlacement('top');
+        }
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll, { passive: true });
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [buttonRef, tooltipPlacement]);
+
   return (
     <ContainerWebsites colors={colors}>
       <div style={{ margin: '0 auto 60px', maxWidth: 320 }}>
@@ -55,7 +75,7 @@ export const WebsitesSection = ({ lightIsOn, colors }) => {
       ))}
       <Tooltip
         message={t('textWebsites.tootlipMessage')}
-        placement="top"
+        placement={tooltipPlacement}
         isVisible={isTooltipVisible}
         targetRef={buttonRef}
         colors={colors}
